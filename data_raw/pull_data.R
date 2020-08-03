b06c2b05ac103074eb073bb6e6d43347aa8e399f
RamiKrispin <ramkrisp@umich.edu>
2020-08-03 00:11:06 -0700

    update the data refresh

--- data_raw/pull_data.R
+++ data_raw/pull_data.R
@@ -38,14 +38,15 @@ covid19swiss <- df_raw %>%
   dplyr::mutate(date = as.Date(date),
                 canton = abbreviation_canton_and_fl) %>%
   dplyr::group_by(date, canton) %>%
-  dplyr::summarise(total_tested = sum(ncumul_tested, na.rm = any(!is.na(ncumul_tested))),
-                   total_confirmed = sum(ncumul_conf, na.rm = any(!is.na(ncumul_conf))),
-                   new_hosp = sum(new_hosp, na.rm = any(!is.na(new_hosp))),
-                   current_hosp = sum(current_hosp, na.rm = any(!is.na(current_hosp))),
-                   current_icu = sum(current_icu, na.rm = any(!is.na(current_icu))),
-                   current_vent = sum(current_vent, na.rm = any(!is.na(current_vent))),
-                   total_recovered = sum(ncumul_released, na.rm = any(!is.na(ncumul_released))),
-                   total_death = sum(ncumul_deceased, na.rm = any(!is.na(ncumul_deceased)))) %>%
+  dplyr::summarise(tested_total = sum(ncumul_tested, na.rm = any(!is.na(ncumul_tested))),
+                   cases_total = sum(ncumul_conf, na.rm = any(!is.na(ncumul_conf))),
+                   hosp_new = sum(new_hosp, na.rm = any(!is.na(new_hosp))),
+                   hosp_current = sum(current_hosp, na.rm = any(!is.na(current_hosp))),
+                   icu_current = sum(current_icu, na.rm = any(!is.na(current_icu))),
+                   vent_current = sum(current_vent, na.rm = any(!is.na(current_vent))),
+                   recovered_total = sum(ncumul_released, na.rm = any(!is.na(ncumul_released))),
+                   deaths_total = sum(ncumul_deceased, na.rm = any(!is.na(ncumul_deceased))),
+                   .groups = "drop") %>%
   tidyr::pivot_longer(c(-date, - canton),
                       names_to = "data_type",
                       values_to = "value") %>%
